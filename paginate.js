fac3c749571ae48857671e0531e575b28a632846
Dan Craig <drcraig@alum.mit.edu>
2012-10-02 17:36:29 -0400

    Progress bar

--- paginate.js
+++ paginate.js
@@ -7,20 +7,34 @@ function getHash() {
   return hash;
 }
 
+function setHash(hash) {
+    if (hash === "#cover")
+    {
+        window.location.hash = "";
+    } else {
+        window.location.hash = hash;
+    }
+}
+
+function setCurrentPage(hash) {
+    $(".page").hide();
+    $(".page"+hash).show();
+    setHash(hash);
+    $("#progress span").removeClass("current");
+    $("#progress #progress-"+hash.slice(1)+" span").addClass("current");
+}
+
 $(document).ready( function() {
-  $(".page").hide();
-  
-  var hash = getHash()
-  $(".page"+hash).show();
+  setCurrentPage(getHash());  
 
   $("#next").click(function(event){
     event.preventDefault();
     var hash = getHash();
     var curr_page = $(".page"+hash);
     var next_page = curr_page.next();
-    curr_page.hide();
-    next_page.show();
-    window.location.hash = next_page.attr('id');
+    if (next_page.length !== 0) {
+      setCurrentPage('#'+next_page.attr('id'));
+    }
   });
 
   $("#prev").click(function(event){
@@ -28,10 +42,21 @@ $(document).ready( function() {
     var hash = getHash();
     var curr_page = $(".page"+hash);
     var prev_page = curr_page.prev();
-    curr_page.hide();
-    prev_page.show();
-    window.location.hash = prev_page.attr('id');
+    if (prev_page.length !== 0) {
+      setCurrentPage('#'+prev_page.attr('id'));
+    }
   });
+
+  $("#progress a").click(function(event){
+    event.preventDefault();
+    setCurrentPage(event.currentTarget.hash); 
+  });
+
+  $("a").click(function(event){
+    event.preventDefault();
+    console.log(event);
+  });
+    
 });
 
 
